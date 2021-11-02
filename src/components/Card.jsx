bbe739f1f18037c4e1e1932a6d7dc1d9f1f4e4f9
ThabatAcosta <91706609+ThabatAcosta@users.noreply.github.com>
2021-11-02 17:42:51 -0500

    tercer commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 const Card = ({title, completed, id, handleClick}) => {
 
     return (
-        <div className=' w-11/12 bg-purple-500 flex flex-col  p-2 border-purple-100 border-4 rounded-xl justify-around  items-center space-y-2  ' style={ {minHeight:'10rem' } }>
-             {/* <div className=' container mx-auto w-full h-1/3 bg-purple-500 flex flex-row  '></div> */}
+        <div className=' w-11/12 bg-gradient-to-r from-green-200 via-green-400 to-green-200 flex flex-col  p-2 border-gray-500 border-2 rounded-xl justify-around  items-center space-y-2  ' style={ {minHeight:'10rem' } }>
           
                 <h3 className=' text-lg text-black text-center capitalize w-full'>{title}</h3>
-                <button type="button" onClick={() => handleClick(id, completed)} className={`font-extrabold ${completed? 'bg-pink-400' : 'bg-yellow-300'} rounded font-serif font-normal p-2 w-full `} >{ completed ? 'Completa' : 'Imcompleta'}</button>
+                <button type="button" onClick={() => handleClick(id, completed)} className={`font-extrabold ${completed? ' bg-black' : ' bg-gray-500'} rounded font-serif font-normal p-2 w-full text-white`} >{ completed ? 'Completa' : 'Incompleta'}</button>
           
         </div>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,11 @@ const Header = ({handleCompletedTasks, handleTasks, handleIcompletedTasks}) => {
 
 
     return (
-        <header className=' bg-purple-900  w-full  flex flex-col items-center justify-center space-y-5 font-serif font-medium py-2' >
+        <header className=' bg-gradient-to-r from-green-200 via-green-400 to-green-200 w-full  flex flex-col items-center justify-center space-y-5 font-serif font-medium py-2' >
 
-            <h1  className=' text-center text-6xl'>¡ToDo!</h1>
+            <h1  className=' text-center text-6xl font-medium'>¡ToDo!</h1>
 
-            <div className=' flex flex-row space-x-10 '>
+            <div className=' flex flex-row space-x-10'>
                <Button title='Completadas' handleClick={handleCompletedTasks}/>
                <Button title='Todas' handleClick={handleTasks}/>
                <Button title='Incompletas' handleClick={handleIcompletedTasks}/>
@@ -25,7 +25,7 @@ const Button = ({handleClick=()=>{} , title=''}) => {
     return(
 
         <>
-        <button type="button" onClick={handleClick} className=' bg-green-500 rounded font-serif font-normal p-3'>{title} </button>
+        <button type="button" onClick={handleClick} className=' bg-green-100 rounded font-serif font-normal p-3 border-gray-500 border-2 shadow-xl'>{title} </button>
         </>
 
 
